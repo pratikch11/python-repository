110fc3f51eb973466054aadde76661350df6a1a4
pratikch11 <pratikchoudhury011@gmail.com>
2025-05-19 22:50:00 +0530

    Add files via upload

--- 9_quiz game.py
+++ 9_quiz game.py
@@ -1,44 +1,35 @@
+import random
+r = random.randint(0,100)
+n = bin(r)[2:]
 score = 0
 total_Question = 0
-l = { 
-    "42":101010,
-    "24":11000,
-    "32":100000,
-    "45":101101,
-    "23":10111,
-    "67":100011,
-    "56":1010110,
-    "34":100010,
-    "78":1001110,
-    "92":1011100}
 print('''Welcome to the Quick Binary revsion.
         It wil recall binary number line.\n\n\n\t
-      ------ All The Best ------\n\n\n\t''')
-print('These are the keys of binary number:-')
-for item in l:
-    print(item)
-while True:
-    a = input('Enter a key given above:- ')
-    a2 = int(input('Enter the binary number:---\nans=\t'))
+        ------ All The Best ------\n\n\n\t''')
+b = int(input('How many quetsion do you want solve: '))
+while True:    
+            c = print(f'convert {r} into binary.')
+            a = input('Enter the binary number: ')
+            
+            if(a == n):
+                print('You are right.KEEP IT UP....')
+                total_Question+=1
+                score+=1
+            else:
+                print(f'You are incorrect.the actual answer is {n} ')
+                total_Question+=1
+            if(total_Question==b):
+                choice = input('your attmept get over.do you want continue [y/n]')
+                if(choice=='n'):
+                    print('program finished')
+                    print(f'''Thank you
+                        score = {int(score)}\\b
+                        total question {int(total_Question)}\\b
+                        wrong question {total_Question - score}\\b
+                        right answer {score}\\b''')
+                    break
+            else:
+                continue       
 
-    if (a2==l[a]):
-            print('Correct! you are given right answer.')
-            score+=1
-            total_Question+=1
-    else:
-        print(f'Incorrect! your answer is {l[a]}')
-        total_Question+=1
-    
-    if(total_Question==4):
-        choice=input("do you wish to continue?[Y/N]")
-        if choice. lower()=="n":
-            print('program finished')
-            print(f'''Thank you
-                        score = {int(score)}
-                        total question {int(total_Question)}
-                        wrong question {total_Question - score}
-                        right answer {score}''')
-            break
-        else:
-            continue  
-      
+
+             
